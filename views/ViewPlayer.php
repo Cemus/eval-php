3b972a1641c75288aca37876e605c06f9cefce38
Cemus <24947582+Cemus@users.noreply.github.com>
2025-02-21 12:14:52 +0100

    Fix form ('style...')

--- views/ViewPlayer.php
+++ views/ViewPlayer.php
@@ -15,10 +15,18 @@ class ViewPlayer{
             <section>
             <h2>Ajouter joueur</h2>
             <form action="" method="post">
+                <label for="pseudo">Pseudo *</label>
                 <input type="text" name="pseudo" placeholder="Le pseudo">
+                </br>
+                <label for="pseudo">Email *</label>
                 <input type="text" name="email" placeholder="L'email">
+                </br>
+                <label for="pseudo">Score</label>
                 <input type="number" name="score" placeholder="Le score">
+                </br>
+                <label for="pseudo">Mot de passe *</label>
                 <input type="password" name="password" placeholder="Le mot de passe">
+                </br>
                 <input type="submit" name="submitAddPlayer">
             </form>
             <p> <?php $this->getSignUpMessage() ?> </p>
