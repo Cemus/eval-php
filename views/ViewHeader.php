5eb1fc3ebff0a08046083dd828889601e3082780
Cemus <24947582+Cemus@users.noreply.github.com>
2025-02-21 12:29:59 +0100

    Add impressive styling

--- views/ViewHeader.php
+++ views/ViewHeader.php
@@ -12,6 +12,30 @@ class ViewHeader{
             <title>Supergame</title>
         </head>
         <style>
+            :root{
+                --main:var(blue);
+                --background:var(#222)
+            }
+            body{
+                display: grid;
+                grid-template-columns: 1fr;
+                grid-template-rows: auto 1fr auto;
+                min-height: 100vh;
+                margin: 0;
+                text-align: center;
+                background-color: #222;
+                color: white;
+            }
+            header {
+                background-color: blue;
+                padding: .5rem;
+                text-align: left;
+            }
+            h2{
+                background-color: blue;
+                padding: .5rem;
+                border-radius: .2rem;
+            }
             main {
                 display:flex;
                 justify-content:space-around;
